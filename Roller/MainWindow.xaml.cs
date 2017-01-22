033eb3cae810aa42f9c5e42885e93bda5dbd388b
Tyler <t.dett@yahoo.com>
2017-01-21 23:16:57 -0500

    Second Commit

--- Roller/MainWindow.xaml.cs
+++ Roller/MainWindow.xaml.cs
@@ -12,21 +12,40 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Sockets;
 
 namespace Roller
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary>dice
     public partial class MainWindow : Window
     {
+        Random r;
+
         public MainWindow()
         {
+            r = new Random();
             InitializeComponent();
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            int result = 0;
+            int diceValue = 0;
+          
+
+            if ((bool)D4.IsChecked) { diceValue = 4; }
+            else if ((bool)D6.IsChecked) { diceValue = 6; }
+            else if ((bool)D8.IsChecked) { diceValue = 8; }
+            else if ((bool)D10.IsChecked) { diceValue = 10; }
+            else if ((bool)D12.IsChecked) { diceValue = 12; }
+            else if ((bool)D20.IsChecked) { diceValue = 20; }
+
+            result = r.Next(1, diceValue);
+
+            DiceValue.Text = "The number rolled was: " + result + "\n";
+
 
         }
     }
